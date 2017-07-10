87c65aa7d6674eab5a40bc0d485bb2091f934742
Jam <guo3jin3cai@gmail.com>
2017-07-10 22:23:57 +0800

    get carts from mongodb

--- client/src/components/cart/cart.vue
+++ client/src/components/cart/cart.vue
@@ -3,19 +3,19 @@
     <div class="cart-cont">
       <ul>
         <li class="cont-one" v-for="(cart, index) in carts">
-          <input class="goods-checkbox" type="checkbox" v-model="cart.isSelect" @click="clickSelect(cart)">
+          <input class="goods-checkbox" type="checkbox" v-model="cart.cart_isSelect" @click="clickSelect(cart)">
           <a class="goods-a" href="javascript:;">
-            <img class="goods-img" :src="cart.cart_img">
+            <img class="goods-img" :src="cart.brand_pic">
           </a>
           <div class="goods-info">
-            <h5 class="goods-name">{{cart.cart_name}}</h5>
+            <h5 class="goods-name">{{cart.brand_name}}</h5>
             <div class="goods-counter">
               <a href="javascript:;" class="btn-sub" @click="changeNum(-1, cart)"> - </a>
               <input type="text" class="goods-num" readonly="readonly" v-model.lazy="cart.cart_num">
               <a href="javascript:;" class="btn-add" @click="changeNum(1, cart)"> + </a>
             </div>
           </div>
-          <span class="goods-price">￥{{cart.cart_price*cart.cart_num}}</span>
+          <span class="goods-price">￥{{cart.brand_price*cart.cart_num}}</span>
           <a class="goods-delete" href="javascript:;" @click="delectCart(index, carts)">删除</a>
         </li>
       </ul>
@@ -35,6 +35,7 @@
 </template>
 
 <script>
+  import Jam from '../com/jam'
   import LocalDB from '../com/localDB'
   import '../../css/cart.scss'
 
@@ -43,7 +44,8 @@
       return {
         dataCart: {},
         carts: {},
-        isAllSelectState: false
+        isAllSelectState: false,
+        jam: function () {}
       }
     },
     created () {
@@ -56,8 +58,8 @@
         // this.carts.forEach(cart => {    // 可以执行，但是报错this.carts.forEach is not a function
         Array.from(this.carts).forEach(cart => {
         // [...this.carts].forEach(cart => {   //SyntaxError: Unexpected token
-          if (cart.isSelect) {
-            price += cart.cart_price * cart.cart_num
+          if (cart.cart_isSelect) {
+            price += cart.brand_price * cart.cart_num
           }
         })
         return price
@@ -72,19 +74,39 @@
         return this.$store.dispatch('changeSideBarState', false)
       },
       getDataCart () {
-        let localDB = new LocalDB('dataCart')
-        if (localDB.get('dataCart').length === 0 || localDB.get('dataCart').data.carts.length === 0) {
-          this.$http.get('../../static/data/cart.json').then((response) => {
-            this.dataCart = response.data
-            this.carts = this.dataCart.data.carts
-            localDB.set(this.dataCart)
-          }, (response) => {
-            // error
+        this.jam = new Jam()
+        let name = this.jam.locDbGet('dataLogin').name
+        this.$http({
+          url: '/api/goods/carts',
+          method: 'GET',
+          params: {
+            name: name
+          }
+        })
+          .then((res) => {
+            let data = res.data
+            console.log(data)
+            if (data.code === 200) {
+              console.log(data.msg)
+              this.dataCart = data
+              this.carts = data.data
+            } else {
+              console.log(data.msg)
+            }
           })
-        } else {
-          this.dataCart = localDB.get('dataCart')
-          this.carts = this.dataCart.data.carts
-        }
+//        let localDB = new LocalDB('dataCart')
+//        if (localDB.get('dataCart').length === 0 || localDB.get('dataCart').data.carts.length === 0) {
+//          this.$http.get('../../static/data/cart.json').then((response) => {
+//            this.dataCart = response.data
+//            this.carts = this.dataCart.data.carts
+//            localDB.set(this.dataCart)
+//          }, (response) => {
+//            // error
+//          })
+//        } else {
+//          this.dataCart = localDB.get('dataCart')
+//          this.carts = this.dataCart.data.carts
+//        }
       },
       changeNum (change, cart) {
         if (change === -1) {
@@ -96,9 +118,9 @@
         }
       },
       clickSelect (cart) {
-        cart.isSelect = !cart.isSelect  // 没有这个，数据没及时同步到全选
+        cart.cart_isSelect = !cart.cart_isSelect  // 没有这个，数据没及时同步到全选
         let isAllSelectState = Array.from(this.carts).every(cart => {
-          return cart.isSelect
+          return cart.cart_isSelect
         })
         this.isAllSelectState = isAllSelectState
       },
@@ -106,7 +128,7 @@
         this.isAllSelectState = !this.isAllSelectState
         Array.from(this.carts).forEach(cart => {
         // [...this.carts].forEach(cart => {   //SyntaxError: Unexpected token
-          cart.isSelect = this.isAllSelectState
+          cart.cart_isSelect = this.isAllSelectState
         })
       },
       delectCart (index, carts) {
